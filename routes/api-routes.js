1e23309608d5706e0f673ff4bde0fc435f04e4ef
Eric Johnson <ericjohnson0216@icloud.com>
2020-11-02 21:03:22 -0500

    Done.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,18 +15,35 @@
 // =============================================================
 module.exports = function(app) {
 
+//4) Add a Sequelize findAll method inside the GET route which finds all of the todos and returns them to the user as JSON.
   // GET route for getting all of the todos
-  app.get("/api/todos", function(req, res) {
+    app.get("/api/todos", function(req, res) {
+      //writing it with sequelize
+        db.Todo.findAll({}).then(function(dbTodo) {
+            res.json(dbTodo);
+    //writing it as ORM   
     // orm.getTodos(function(results) {
     //   res.json(results);
+        });
     });
   
-
+    //5) Add a Sequelize create method to the POST route to save a new todo to the database using the data sent to the server in req.body.
   // POST route for saving a new todo. We can create a todo using the data on req.body
-  app.post("/api/todos", function(req, res) {
+    app.post("/api/todos", function(req, res) {
+      // create takes an argument of an object describing the item we want to
+    // insert into our table. In this case we just we pass in an object with a text
+    // and complete property
+        db.Todo.create({
+        text: req.body.text,
+        complete: req.body.complete
+    }).then(function(dbTodo) {
+      // We have access to the new todo as an argument inside of the callback function
+      res.json(dbTodo);
+    
     // orm.addTodo(req.body, function(results) {
     //   res.json(results);
     });
+});
   
 
   // DELETE route for deleting todos. We can access the ID of the todo to delete in
